bedb9d94235c3429caa5dbb7393a39a09a7d179e
j-whiteside <whiteside.jeremy@gmail.com>
2016-03-11 23:58:26 -0500

    Division and addition do not work properly

--- app/src/main/java/com/example/student/calculatr/MainActivity.java
+++ app/src/main/java/com/example/student/calculatr/MainActivity.java
@@ -14,6 +14,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //These global variables control the live inputs and those stored in memory, the most recent arithmetic operator.
+
     public static final String CLEARED_DISPLAY_STRING = "0";
     public String currentDisplayString = "0";
     public double currentDisplayDouble = 0;
@@ -33,8 +35,10 @@ public class MainActivity extends AppCompatActivity {
         displayView = (TextView) findViewById(R.id.displayView);
     }
 
-    public void clearDisplay(View v)
+    public void clearDisplay(View inputButton)
     {
+        //This function is called when the user presses the "Clear" button.
+        //It resets all globabl variables to their default state, and clears the display
         currentDisplayString = CLEARED_DISPLAY_STRING;
         firstNumberBoolean = true;
         negativeNumberBoolean = false;
@@ -47,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void numberInputHandler(View inputButton)
     {
+        //This function handles the input of any numbers.  Whenever a number is pressed on the numpad,
+        //this function will determine the numerical value of that button, and add it to the display.
+        //It also has logic to prevent leading zeroes when inputing numbers
         Button input = (Button) inputButton;
         if (firstNumberBoolean==true)
         {
@@ -56,13 +63,15 @@ public class MainActivity extends AppCompatActivity {
 
 
         currentDisplayString += input.getText();
-        mostRecentInputDouble = currentDisplayDouble;
+        mostRecentInputDouble = Double.parseDouble(currentDisplayString);
         updateDisplay();
 
     }
 
     public void eraseCharacter(View inputButton)
     {
+        //This function handles the erase button.  When it is pressed, this function will erase the last number on the display.
+        //When the last number is erased, the display is reset to 0
         if (currentDisplayString.length() > 1)
         {
             currentDisplayString = currentDisplayString.substring(0, currentDisplayString.length()-1);
@@ -78,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void decimalInputHandler(View inputButton)
     {
+        //This function handles the decimal button.  It ensures that there can only be one decimal on the display at any time.
+        //If there is a decimal, it will prevent the addition of a second one
         Button input = (Button) inputButton;
         if (!currentDisplayString.contains("."))
         {
@@ -90,13 +101,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void negativeInputHandler(View inputButton)
     {
+        //This function handles the negative/positive button.  If the number in the display is positive, it will add a minus symbol infront of it.
+        //If the number is already negative, it will remove the minus symbol
+
+        //If there is no number on the display, it will not input a minus symbol
         if (firstNumberBoolean == false)
         {
+            //If the number displayed is positive
             if (negativeNumberBoolean == false)
             {
                 currentDisplayString = "-" + currentDisplayString;
                 negativeNumberBoolean = true;
-            } else
+            }
+
+            //If the number is already negative
+            else
             {
                 currentDisplayString = currentDisplayString.replaceAll("-", "");
                 negativeNumberBoolean = false;
@@ -109,33 +128,48 @@ public class MainActivity extends AppCompatActivity {
 
     public void operatorInputHandler(View inputButton)
     {
+        //This function handles the arithmetic operators, and the equals button.
+        //It has separate logic for the operators and for the equals button.
+
+
         Button input = (Button) inputButton;
-        String buttonText = (String) input.getText();
-        if (buttonText.contains("="))
+        String buttonText = (String) input.getTag();
+
+        //If the input is not and equals sign, it is either +, -, X, /
+        if (!buttonText.equals("equals"))
         {
             previousDisplayDouble = Double.parseDouble(currentDisplayString);
             arithmeticOperatorString = buttonText;
             negativeNumberBoolean = false;
         }
 
+        //If the input is an equals sign
         else
         {
             currentDisplayDouble = Double.parseDouble(currentDisplayString);
 
             switch (arithmeticOperatorString) {
-                case "/":
-                    currentDisplayDouble = (previousDisplayDouble / mostRecentInputDouble);
+
+                    //If the most recent arithmetic operator is division
+                case "division":
+                    currentDisplayDouble = (previousDisplayDouble/mostRecentInputDouble);
                     currentDisplayString = String.valueOf(currentDisplayDouble);
                     previousDisplayDouble = Double.parseDouble(currentDisplayString);
-                case "X":
+
+                    //If the most recent arithmetic operator is multiplication
+                case "multiplication":
                     currentDisplayDouble = (previousDisplayDouble * mostRecentInputDouble);
                     currentDisplayString = String.valueOf(currentDisplayDouble);
                     previousDisplayDouble = Double.parseDouble(currentDisplayString);
-                case "+":
-                    currentDisplayDouble = (previousDisplayDouble + mostRecentInputDouble);
+
+                    //If the most recent arithmetic operator is addition
+                case "addition":
+                    currentDisplayDouble = (previousDisplayDouble+mostRecentInputDouble);
                     currentDisplayString = String.valueOf(currentDisplayDouble);
                     previousDisplayDouble = Double.parseDouble(currentDisplayString);
-                case "-":
+
+                    //If the most recent arithmetic operator is subtraction
+                case "subtraction":
                     currentDisplayDouble = (previousDisplayDouble - mostRecentInputDouble);
                     currentDisplayString = String.valueOf(currentDisplayDouble);
                     previousDisplayDouble = Double.parseDouble(currentDisplayString);
@@ -149,6 +183,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateDisplay()
     {
+        //This function updates the display with the contents of currentDisplayString
+        //It is called every time the user enters a number, clears the display, deletes a digit, or presses the equals button.
         displayView.setText(currentDisplayString);
     }
 
